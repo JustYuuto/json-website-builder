2105c488169629e12ef018fc972824365ee8262f
NetherMC <thelio.trinite@outlook.com>
2021-11-27 18:24:03 +0100

    types

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+export default class JSONWebsiteBuilder {
+    public constructor(json: Object);
+}
+
+export class ChildrenTags {
+    private childrenTags: string[];
+    public constructor();
+    static isChildrenTag(tag: string): boolean;
+}
